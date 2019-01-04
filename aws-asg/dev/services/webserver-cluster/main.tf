21ed816cae8239786013f786ea3921691d99f3aa
nicolasduminil <nicolas.duminil@simplex-software.fr>
2019-01-04 15:50:27 +0100

    Have corrected some variable names

--- aws-asg/dev/services/webserver-cluster/main.tf
+++ aws-asg/dev/services/webserver-cluster/main.tf
@@ -7,13 +7,15 @@ provider "aws" {
 }
 
 module "webserver_cluster" {
-  source = "https://github.com/nicolasduminil/aws-web-server-cluster-with-terraform-module.git?ref=dev.v0.0.1"
-  cluster_name = "webserver-cluster-dev"
-  db_remote_state_bucket = "${var.db_remote_state_bucket}"
-  db_remote_state_key = "${var.db_remote_state_key}"
-  instance_type = "t2.micro"
-  image_id = "ami-40d28157"
-  min_size = 2
-  max_size = 2
+  //source = "git@github.com:nicolasduminil/aws-web-server-cluster-with-terraform-module.git"
+  source = "../../../modules/services/webserver-cluster/"
+  web_server_cluster_name = "webserver-cluster-dev"
+  s3_bucket_name_for_db_state = "${var.s3_bucket_name_for_db_state}"
+  s3_path_for_db_state = "${var.s3_path_for_db_state}"
+  ec2_instance_type = "t2.micro"
+  ami_image_id = "ami-40d28157"
+  ec2_instance_min_size = 2
+  ec2_instance_max_size = 2
+  environment_name = "dev"
 }
 
